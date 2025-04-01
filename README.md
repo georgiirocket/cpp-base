68ade3503b9274432bf076f9a172fd36cafbd54e
g_rocket <georgiiromanishko@gmail.com>
2025-04-01 11:35:01 +0200

    feat(project): class extends

--- README.md
+++ README.md
@@ -255,3 +255,53 @@ int main() {
     return 0;
 }
 ```
+
+## Class extends
+
+```c++
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    string color;
+
+    Shape(string color) {
+        this->color = color;
+    }
+
+    ~Shape() {
+        cout << "Descructor shape" << endl;
+    }
+    
+    void info() {
+        cout << "Color: " << color << endl;
+    }
+};
+
+//public Shape, protected Shape, privat shape
+class Circle : public Shape {
+public: 
+    int radius;
+
+    Circle(string color, int radius) : Shape(color) {
+        this->radius = radius;
+    }
+
+    ~Circle() {
+        cout << "Descructor circle" << endl;
+    }
+
+    void print() {
+        this->info();
+        cout << "Radius: " << this->radius << endl;
+    }
+};
+
+int main() {
+    Circle c("red", 10);
+
+    c.print();
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 using namespace std;
 
-enum Day {
-    Sunday,
-    Monday
+class Shape {
+public:
+    string color;
+
+    Shape(string color) {
+        this->color = color;
+    }
+
+    ~Shape() {
+        cout << "Descructor shape" << endl;
+    }
+    
+    void info() {
+        cout << "Color: " << color << endl;
+    }
+};
+
+//public Shape, protected Shape, privat shape
+class Circle : public Shape {
+public: 
+    int radius;
+
+    Circle(string color, int radius) : Shape(color) {
+        this->radius = radius;
+    }
+
+    ~Circle() {
+        cout << "Descructor circle" << endl;
+    }
+
+    void print() {
+        this->info();
+        cout << "Radius: " << this->radius << endl;
+    }
 };
 
 int main() {
-    Day one = Sunday;
+    Circle c("red", 10);
 
-    cout << one << endl;
-
+    c.print();
     return 0;
 }
