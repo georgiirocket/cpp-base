6ddf37759ac1d8f442db76401df8afc2253e5e08
g_rocket <georgiiromanishko@gmail.com>
2025-04-02 14:24:41 +0200

    feat(project): is circle

--- main.cpp
+++ main.cpp
@@ -3,26 +3,75 @@
 #include <typeinfo>
 using namespace std;
 
-class Author {
+class Shape {
     public:
-        Author(const string& name) : name(name) {}
-    
-    private: 
-        string name;    
+        virtual int calculateArea() = 0;
+        virtual int calculatePerimeter() = 0;
+        virtual string getTypeName() = 0;
 };
-
-class Book {
-    private:
-        string title;
-        Author& author;
     
+class Circle : public Shape {
+    private:
+        int radius;
+        
     public:
-        Book(const string& title, Author& author): title(title), author(author) {}    
+        Circle(int r) {
+            radius = r;
+        }
+        
+        int calculateArea() override {
+            return 3.14 * radius * radius;
+        }
+        
+        int calculatePerimeter() override {
+            return 2 * 3.14 * radius;
+        }
+        
+        string getTypeName() override {
+            return typeid(*this).name();
+        }
+};
+    
+class Rectangle : public Shape {
+    private:
+        int a;
+        int b;
+        
+    public:
+        Rectangle(int a, int b) {
+            this->a = a;
+            this->b = b;
+        }
+        
+        int calculateArea() override {
+            return a * b;
+        }
+        
+        int calculatePerimeter() override {
+            return (a + b) * 2;
+        }
+
+        string getTypeName() override {
+            return typeid(*this).name();
+        }
+    };
+    
+bool isCircle(Shape* shape) {
+    const string str = "Circle";
+
+    if(shape->getTypeName().find(str) != std::string::npos) {
+        return true;
+    }
+
+    return false;
 };
 
 int main() {
-    Author author("Patrik");
-    Book book("My book", author);
+    Shape* c = new Circle(10);
+    Shape* r = new Rectangle(10, 5);
+    
+    cout << isCircle(c) << endl;
+    cout << isCircle(r) << endl;
 
 
     return 0;
