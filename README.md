e388df8f1082c0d98d772bf93deff9a598306e95
g_rocket <georgiiromanishko@gmail.com>
2025-04-04 13:35:30 +0200

    feat(project): template class

--- README.md
+++ README.md
@@ -736,3 +736,64 @@ int main() {
     return 0;
 }
 ```
+
+## Template class
+
+```c++
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+struct Node
+{
+    T val;
+    Node* next;
+};
+
+template <typename T>
+class Stack {
+    private: 
+        Node<T>* head;
+
+    public:
+        Stack() : head(nullptr) {}
+        
+        void push(T val) {
+            Node<T>* newHead = new Node<T>{val, head};
+            head = newHead;
+        }
+
+        T pop() {
+            T res = head->val;
+            Node<T>* temp = head->next;
+
+            delete head;
+            head = temp;
+
+            return res;
+        }
+};
+
+int main() {
+    Stack<double> stack;
+
+    for(int i = 0; i < 5; ++i) {
+        stack.push(i / 2.0);
+    }
+
+    for(int i = 0; i < 5; ++i) {
+        cout << stack.pop() << endl;
+    }
+
+    Stack<char> stack1;
+
+    for(int i = 0; i < 5; ++i) {
+        stack1.push(char('a' + i));
+    }
+
+    for(int i = 0; i < 5; ++i) {
+        cout << stack1.pop() << endl;
+    }
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,15 +2,55 @@
 #include <string>
 using namespace std;
 
-template<typename T>
+template <typename T>
+struct Node
+{
+    T val;
+    Node* next;
+};
 
-T Max(T a, T b) {
-    return a > b ? a : b;
-}
+template <typename T>
+class Stack {
+    private: 
+        Node<T>* head;
+
+    public:
+        Stack() : head(nullptr) {}
+        
+        void push(T val) {
+            Node<T>* newHead = new Node<T>{val, head};
+            head = newHead;
+        }
+
+        T pop() {
+            T res = head->val;
+            Node<T>* temp = head->next;
+
+            delete head;
+            head = temp;
+
+            return res;
+        }
+};
 
 int main() {
-    cout << Max(5, 6) << endl;
-    cout << Max(5.25, 5.27) << endl;
+    Stack<double> stack;
 
-    return 0;
+    for(int i = 0; i < 5; ++i) {
+        stack.push(i / 2.0);
+    }
+
+    for(int i = 0; i < 5; ++i) {
+        cout << stack.pop() << endl;
+    }
+
+    Stack<char> stack1;
+
+    for(int i = 0; i < 5; ++i) {
+        stack1.push(char('a' + i));
+    }
+
+    for(int i = 0; i < 5; ++i) {
+        cout << stack1.pop() << endl;
+    }
 }
