2f41f9882500a95a38b06935d16857e28bb2290f
g_rocket <georgiiromanishko@gmail.com>
2025-04-04 14:01:28 +0200

    feat(project): template pair

--- README.md
+++ README.md
@@ -797,3 +797,54 @@ int main() {
     }
 }
 ```
+
+## Template pair
+
+```c++
+template <typename T, typename P>
+struct Node
+{
+    T val1;
+    P val2;
+    Node* next;
+};
+
+template <typename T, typename P>
+class Stack {
+    private: 
+        Node<T, P>* head;
+
+    public:
+        Stack() : head(nullptr) {}
+        
+        void push(T val1, P val2) {
+            Node<T, P>* newHead = new Node<T, P>{val1, val2, head};
+            head = newHead;
+        }
+
+        pair<T, P> pop() {
+            pair<T, P> res = {head->val1, head->val2};
+            P res1 = head->val2;
+            Node<T, P>* temp = head->next;
+
+            delete head;
+            head = temp;
+
+            return res;
+        }
+};
+
+int main() {
+    Stack<char, int> stack;
+
+    for(int i = 0; i < 5; ++i) {
+        stack.push(char('a' + i), i);
+    }
+
+    for(int i = 0; i < 5; ++i) {
+        pair<char, int> p = stack.pop();
+
+        cout << p.first << p.second << endl;
+    }
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,29 +2,31 @@
 #include <string>
 using namespace std;
 
-template <typename T>
+template <typename T, typename P>
 struct Node
 {
-    T val;
+    T val1;
+    P val2;
     Node* next;
 };
 
-template <typename T>
+template <typename T, typename P>
 class Stack {
     private: 
-        Node<T>* head;
+        Node<T, P>* head;
 
     public:
         Stack() : head(nullptr) {}
         
-        void push(T val) {
-            Node<T>* newHead = new Node<T>{val, head};
+        void push(T val1, P val2) {
+            Node<T, P>* newHead = new Node<T, P>{val1, val2, head};
             head = newHead;
         }
 
-        T pop() {
-            T res = head->val;
-            Node<T>* temp = head->next;
+        pair<T, P> pop() {
+            pair<T, P> res = {head->val1, head->val2};
+            P res1 = head->val2;
+            Node<T, P>* temp = head->next;
 
             delete head;
             head = temp;
@@ -34,23 +36,15 @@ class Stack {
 };
 
 int main() {
-    Stack<double> stack;
+    Stack<char, int> stack;
 
     for(int i = 0; i < 5; ++i) {
-        stack.push(i / 2.0);
+        stack.push(char('a' + i), i);
     }
 
     for(int i = 0; i < 5; ++i) {
-        cout << stack.pop() << endl;
-    }
+        pair<char, int> p = stack.pop();
 
-    Stack<char> stack1;
-
-    for(int i = 0; i < 5; ++i) {
-        stack1.push(char('a' + i));
-    }
-
-    for(int i = 0; i < 5; ++i) {
-        cout << stack1.pop() << endl;
+        cout << p.first << p.second << endl;
     }
 }
