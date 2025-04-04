c62445243615f4c8f08a28f6fe1d8282e4510f57
g_rocket <georgiiromanishko@gmail.com>
2025-04-04 16:21:46 +0200

    feat(project): template specialization

--- README.md
+++ README.md
@@ -848,3 +848,28 @@ int main() {
     }
 }
 ```
+
+## Template specialization
+
+```c++
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+T Max(T a, T b) {
+    return a > b ? a : b;
+}
+
+template <>
+char Max<char>(char a, char b) {
+    return a > b ? toupper(a) : toupper(a);
+}
+
+int main() {
+    cout << Max(10, 2) << endl;
+    cout << Max('a', 'b') << endl;
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,49 +2,19 @@
 #include <string>
 using namespace std;
 
-template <typename T, typename P>
-struct Node
-{
-    T val1;
-    P val2;
-    Node* next;
-};
+template <typename T>
+T Max(T a, T b) {
+    return a > b ? a : b;
+}
 
-template <typename T, typename P>
-class Stack {
-    private: 
-        Node<T, P>* head;
-
-    public:
-        Stack() : head(nullptr) {}
-        
-        void push(T val1, P val2) {
-            Node<T, P>* newHead = new Node<T, P>{val1, val2, head};
-            head = newHead;
-        }
-
-        pair<T, P> pop() {
-            pair<T, P> res = {head->val1, head->val2};
-            P res1 = head->val2;
-            Node<T, P>* temp = head->next;
-
-            delete head;
-            head = temp;
-
-            return res;
-        }
-};
+template <>
+char Max<char>(char a, char b) {
+    return a > b ? toupper(a) : toupper(a);
+}
 
 int main() {
-    Stack<char, int> stack;
+    cout << Max(10, 2) << endl;
+    cout << Max('a', 'b') << endl;
 
-    for(int i = 0; i < 5; ++i) {
-        stack.push(char('a' + i), i);
-    }
-
-    for(int i = 0; i < 5; ++i) {
-        pair<char, int> p = stack.pop();
-
-        cout << p.first << p.second << endl;
-    }
+    return 0;
 }
