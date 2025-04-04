cb7ca9e8fc82185c1f2772e6dddf4ae90e9af793
g_rocket <georgiiromanishko@gmail.com>
2025-04-04 13:10:25 +0200

    feat(project): templates

--- README.md
+++ README.md
@@ -715,3 +715,24 @@ int main() {
     return 0;
 }
 ```
+
+## Templates
+
+```c++
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+
+T Max(T a, T b) {
+    return a > b ? a : b;
+}
+
+int main() {
+    cout << Max(5, 6) << endl;
+    cout << Max(5.25, 5.27) << endl;
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,134 +2,15 @@
 #include <string>
 using namespace std;
 
-class FranctionalNumber {
-    private: 
-        int numerator;
-        int denominator;
+template<typename T>
 
-    public:
-        FranctionalNumber() {}
-        FranctionalNumber(int num, int den) : numerator(num), denominator(den) {}
-
-    FranctionalNumber operator+(const FranctionalNumber& other) {
-        int num = this->numerator * other.denominator + this->denominator * other.numerator;
-        int den = this->denominator * other.denominator;
-
-        return FranctionalNumber(num, den);
-    }
-
-    FranctionalNumber operator-(const FranctionalNumber& other) {
-        int num = this->numerator * other.denominator - this->denominator * other.numerator;
-        int den = this->denominator * other.denominator;
-
-        return FranctionalNumber(num, den);
-    }
-
-    FranctionalNumber operator*(const FranctionalNumber& other) {
-        int num = this->numerator *  other.numerator;
-        int den = this->denominator * other.denominator;
-
-        return FranctionalNumber(num, den);
-    }
-
-    FranctionalNumber operator/(const FranctionalNumber& other) {
-        int num = this->numerator *  other.denominator;
-        int den = this->denominator * other.numerator;
-
-        return FranctionalNumber(num, den);
-    }
-
-    bool operator==(const FranctionalNumber& other) {
-        return this->numerator == other.numerator && this->denominator == other.denominator;
-    }
-
-    bool operator!=(const FranctionalNumber& other) {
-        return this->numerator != other.numerator || this->denominator != other.denominator;
-    }
-
-    bool operator>(const FranctionalNumber& other) {
-        if(this->denominator == other.denominator) {
-            return this->numerator > other.numerator;
-        }
-
-        int initNumerator = this->numerator * other.denominator;
-        int otherNumerator = other.numerator * this->denominator;
-
-        return initNumerator > otherNumerator;
-    }
-
-    bool operator>=(const FranctionalNumber& other) {
-        if(this->denominator == other.denominator) {
-            return this->numerator >= other.numerator;
-        }
-
-        int initNumerator = this->numerator * other.denominator;
-        int otherNumerator = other.numerator * this->denominator;
-
-        return initNumerator >= otherNumerator;
-    }
-
-    bool operator<(const FranctionalNumber& other) {
-        if(this->denominator == other.denominator) {
-            return this->numerator < other.numerator;
-        }
-
-        int initNumerator = this->numerator * other.denominator;
-        int otherNumerator = other.numerator * this->denominator;
-
-        return initNumerator < otherNumerator;
-    }
-
-    bool operator<=(const FranctionalNumber& other) {
-        if(this->denominator == other.denominator) {
-            return this->numerator <= other.numerator;
-        }
-
-        int initNumerator = this->numerator * other.denominator;
-        int otherNumerator = other.numerator * this->denominator;
-
-        return initNumerator <= otherNumerator;
-    }
-
-    FranctionalNumber operator++() {
-        this->numerator += this->denominator;
-
-        return *this;
-    }
-
-    FranctionalNumber operator++(int) {
-        FranctionalNumber temp = *this;
-
-        this->numerator += this->denominator;
-
-        return temp;
-    }
-
-    friend ostream& operator<<(ostream& out, FranctionalNumber& f);
-    friend istream& operator>>(istream& in, FranctionalNumber& f);
-};
-
-ostream& operator<<(ostream& out, FranctionalNumber& f) {
-    out << f.numerator << " / " << f.denominator;
-
-    return out;
-}
-
-
-istream& operator>>(istream& in, FranctionalNumber& f) {
-    char delim;
-    in >> f.numerator >> delim >> f.denominator;
-
-    return in;
+T Max(T a, T b) {
+    return a > b ? a : b;
 }
 
 int main() {
-    FranctionalNumber f1(5, 6);
-    FranctionalNumber f2(5, 6);
-    FranctionalNumber f3 = f1 * f2;
-    f3 = f3 / f2;
-
-    cout << (f1 != f2) << endl;
+    cout << Max(5, 6) << endl;
+    cout << Max(5.25, 5.27) << endl;
 
     return 0;
 }
