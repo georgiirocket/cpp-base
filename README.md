7e4f2507f89341740ca2a943b15a2952ca5006f3
g_rocket <georgiiromanishko@gmail.com>
2025-04-06 13:09:52 +0200

    feat(project): exceptions

--- README.md
+++ README.md
@@ -1020,3 +1020,27 @@ int main() {
     return 0;
 }
 ```
+
+## Exceptions
+
+```c++
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 10, b = 0;
+
+    try {
+        if(b == 0) {
+            throw runtime_error("Cannot divide by 0");
+        }
+
+        cout << a / b << endl;
+    } catch(const exception& e) {
+        cout << e.what() << endl;
+    }
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,50 +2,17 @@
 #include <string>
 using namespace std;
 
-template <typename T>
-struct Node {
-    T val;
-    unique_ptr<Node<T>> next;
+int main() {
+    int a = 10, b = 0;
 
-    Node(T v, unique_ptr<Node<T>> n) : val(v), next(move(n)) {
-        
-    }
-};
-
-template <typename T>
-class Stack {
-private:
-    unique_ptr<Node<T>> head;
-
-public:
-    Stack() = default;
-
-    void push(T val) {
-        head = make_unique<Node<T>>(val, move(head));
-    }
-
-    T pop() {
-        if (!head) {
-            throw runtime_error("Stack underflow: Cannot pop from empty stack");
+    try {
+        if(b == 0) {
+            throw runtime_error("Cannot divide by 0");
         }
 
-        T res = head->val;
-        head = move(head->next);  // this deletes the old head automatically
-        return res;
-    }
-};
-
-
-
-int main() {
-    Stack<double> stack;
-
-    for(int i = 0; i < 5; ++i) {
-        stack.push(i / 2.0);
-    }
-
-    for(int i = 0; i < 5; ++i) {
-        cout << stack.pop() << endl;
+        cout << a / b << endl;
+    } catch(const exception& e) {
+        cout << e.what() << endl;
     }
 
     return 0;
