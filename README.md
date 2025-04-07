428b5ccec667cb652712c604caef1d79beb9fea9
g_rocket <georgiiromanishko@gmail.com>
2025-04-07 10:00:53 +0200

    feat(project): stack with exception

--- README.md
+++ README.md
@@ -1044,3 +1044,63 @@ int main() {
     return 0;
 }
 ```
+
+## Stack with exception
+
+```c++
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+struct Node {
+    T val;
+    unique_ptr<Node<T>> next;
+
+    Node(T v, unique_ptr<Node<T>> n) : val(v), next(move(n)) {
+        
+    }
+};
+
+template <typename T>
+class Stack {
+private:
+    unique_ptr<Node<T>> head;
+
+public:
+    Stack() = default;
+
+    void push(T val) {
+        head = make_unique<Node<T>>(val, move(head));
+    }
+
+    T pop() {
+        if (!head) {
+            throw overflow_error("Stack underflow: Cannot pop from empty stack");
+        }
+
+        T res = head->val;
+        head = move(head->next);  // this deletes the old head automatically
+        return res;
+    }
+};
+
+
+
+int main() {
+    Stack<double> stack;
+
+    try
+    {
+        stack.pop();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -2,18 +2,54 @@
 #include <string>
 using namespace std;
 
-int main() {
-    int a = 10, b = 0;
+template <typename T>
+struct Node {
+    T val;
+    unique_ptr<Node<T>> next;
 
-    try {
-        if(b == 0) {
-            throw runtime_error("Cannot divide by 0");
+    Node(T v, unique_ptr<Node<T>> n) : val(v), next(move(n)) {
+        
+    }
+};
+
+template <typename T>
+class Stack {
+private:
+    unique_ptr<Node<T>> head;
+
+public:
+    Stack() = default;
+
+    void push(T val) {
+        head = make_unique<Node<T>>(val, move(head));
+    }
+
+    T pop() {
+        if (!head) {
+            throw overflow_error("Stack underflow: Cannot pop from empty stack");
         }
 
-        cout << a / b << endl;
-    } catch(const exception& e) {
-        cout << e.what() << endl;
+        T res = head->val;
+        head = move(head->next);  // this deletes the old head automatically
+        return res;
     }
+};
+
+
+
+int main() {
+    Stack<double> stack;
+
+    try
+    {
+        stack.pop();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    
 
     return 0;
 }
