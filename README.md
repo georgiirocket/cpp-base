883736f3e8793c3d93542770529c3c7530b8ba84
g_rocket <georgiiromanishko@gmail.com>
2025-04-10 09:50:44 +0200

    feat(project): sort with lamda fn

--- README.md
+++ README.md
@@ -1407,3 +1407,27 @@ int main() {
     return 0;
 }
 ```
+
+## Sort with Lambda fn
+
+```c++
+#include <iostream>
+#include <vector>
+#include <algorithm> // for std::sort
+
+int main() {
+    std::vector<int> numbers = {1, 5, 3, 4, 2};
+
+    // Lambda for custom sorting in descending order
+    std::sort(numbers.begin(), numbers.end(), [](int a, int b) {
+        return b < a;
+    });
+
+    for (int n : numbers) {
+        std::cout << n << ' ';
+    }
+    std::cout << '\n';  // Output: 5 4 3 2 1
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <algorithm> // for std::sort
 
 int main() {
-    int x = 10;
+    std::vector<int> numbers = {1, 5, 3, 4, 2};
 
-    auto increment = [x]() mutable {
-        x++;
-        std::cout << x << '\n';  // Output: 11
-    };
+    // Lambda for custom sorting in descending order
+    std::sort(numbers.begin(), numbers.end(), [](int a, int b) {
+        return b < a;
+    });
 
-    increment();
-    std::cout << x << '\n';  // Output: 10
+    for (int n : numbers) {
+        std::cout << n << ' ';
+    }
+    std::cout << '\n';  // Output: 5 4 3 2 1
 
     return 0;
 }
